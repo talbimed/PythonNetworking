3ccb3300e538b0ca045b4aacd3fee184444c7082
talbimed <talbimed@outlook.fr>
2022-02-20 13:41:10 +0100

    Update Port Scan

--- PortScan/PortScan.py
+++ PortScan/PortScan.py
@@ -1,4 +1,27 @@
 
+import socket 
 
 
-print("Hello World")
+
+sock= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = "192.168.1.1"
+
+port =53
+
+def portscanner(port):
+    
+    if (sock.connect_ex((host,port))):
+        print "port %d is colose" %port
+    else:
+        print "port %d is open" % port
+        
+
+
+portscanner(port)
+
+
+
+
+
+
